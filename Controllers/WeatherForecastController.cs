a2d74f6b6521acbca9dd2c0ec8abb10d141d6562
Neville Tummon <neville.tummon@isentia.com>
2020-07-12 01:35:44 +1000

    test out logging

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace TestApi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("testing logging!!");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
